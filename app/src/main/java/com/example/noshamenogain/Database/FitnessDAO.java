f9a3966a44cc96831761365c16057656986d72df
devma <TheLippyT>
2020-02-17 15:35:57 -0300

    Initial Commit. added DAO file with SQL code for the 'Users' table and the users entity package.

---
+++ app/src/main/java/com/example/noshamenogain/Database/FitnessDAO.java
@@ -0,0 +1,76 @@
+package com.example.noshamenogain.Database;
+
+import android.content.Context;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.noshamenogain.Entity.Users;
+
+public class FitnessDAO extends SQLiteOpenHelper {
+    //made into a constant to avoid rewriting them. to avoid types.
+
+    private static final String DATABASE_NAME = "fitness.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public static final String ID = "id";
+    public static final String USERS_TABLE = "users";
+    public static final String USERS_USERNAME = "username";
+    public static final String USERS_PASSWORD = "password";
+    public static final String USERS_WEIGHT = "weight";
+    public static final String USERS_HEIGHT = "height";
+    public static final String USERS_BMI = "bmi";
+
+    private static final String SQL_USERS_TABLE_QUERY = "CREATE TABLE users(id INTEGER PRIMARY KEY, username STRING NOT NULL UNIQUE, password STRING NOT NULL, weight DOUBLE, height DOUBLE, bmi DOUBLE)";
+
+
+    public FitnessDAO(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    private void setDefaultCredentials() {
+        Users users = findOneRecordByUsername("admin");
+        if (users != null) {
+            return;
+        }
+        //Set default username and password
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(USERS_USERNAME, "admin");
+        contentValues.put(USERS_PASSWORD, "admin");
+        insertOneRecord(USERS_TABLE, contentValues);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_USERS_TABLE_QUERY);
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }//content value only for 1 record
+    public long insertOneRecord(String tableName, ContentValues contentValues) {
+        SQLiteDatabase db = getWritableDatabase();
+        long rowId = db.insert(tableName, null, contentValues);
+        db.close();
+        //return the row ID of the newly inserted row, or -1 if an error occurred
+        return rowId;
+    }
+
+    public Users findOneRecordByUsername(String username) {
+        Users user = null;
+        SQLiteDatabase db = getReadableDatabase();
+        String sql = String.format("select * from %s", USERS_TABLE);
+        Cursor cursor = db.rawQuery(sql, null);
+        if (cursor.moveToFirst()) {
+            user = new Users(cursor.getInt(0), cursor.getString(1), cursor.getString(2));
+        }
+        db.close();
+        return user;
+    }
+}
---
+++ app/src/main/java/com/example/noshamenogain/Entity/Users.java
@@ -0,0 +1,66 @@
+package com.example.noshamenogain.Entity;
+
+public class Users {
+    private int id;
+    private String username;
+    private String password;
+    private double weight;
+    private double height;
+
+    public Users(int anInt, String string, String string1) {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getBmi() {
+        return bmi;
+    }
+
+    public void setBmi(double bmi) {
+        this.bmi = bmi;
+    }
+
+    private double bmi;
+
+
+
+}
